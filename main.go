e3b0ec7052096e2028e7aa523d154c5b6e63142b
Andy <ketrifiko@gmail.ccom>
2025-10-10 12:45:03 +0300

    test1 str if num

--- main.go
+++ main.go
@@ -2,6 +2,32 @@ package main
 
 import "fmt"
 
+func sum(a int, b int) int {
+	fmt.Println("a:", a)
+	fmt.Println("b:", b)
+	fmt.Println("_______________________")
+	s := a + b
+	return s
+}
+
+func sumString(s1 string, s2 string) string {
+	s := s1 + s2
+
+	return s
+}
 func main() {
-	fmt.Println("Hello world")
+	number := sum(11, 2)
+	fmt.Printf("Вы получили число: %d\n", number)
+	fmt.Println("_______________________")
+
+	text1 := "Привет "
+	text2 := "Мир!"
+
+	if number < 10 {
+		fmt.Printf("Вы ввели корректный диапозон чисел < 10, число: %d\n", number)
+		text3 := sumString(text1, text2)
+		fmt.Println(text3)
+	} else {
+		fmt.Println("Вы не попали в дапозон числе < 10")
+	}
 }
